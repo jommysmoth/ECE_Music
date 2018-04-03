2db579744f96948f05e3d25c7372befbe569f349
James <smittyprz94@gmail.com>
2018-04-03 00:16:07 -0400

    Mostly ready for data

--- CNN.py
+++ CNN.py
@@ -1,12 +1,14 @@
 """
 Starting Net.
 
-Using Pytorch, unless I have some issues
+Working, ready for data input, need to look into batch minipulation (multiple dim output)
 """
+import numpy as np
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
+import torch.optim as optim
 
 
 class Net(nn.Module):
@@ -49,24 +51,37 @@ class Net(nn.Module):
         Moving the image through the convulutions
         have two maxpools for changing shape
         """
-        inner = self.conv1(input)
-        inner = F.relu(inner)
+        inner = F.relu(self.conv1(input))
         inner = self.maxpool1(inner)
-        inner = self.conv2(inner)
-        inner = F.relu(inner)
+        inner = F.relu(self.conv2(inner))
         inner = self.maxpool2(inner)
         in_shape = inner.view(inner.numel()).size()[0]
         mid_shape = int(in_shape / 5)
         dense_in = nn.Linear(in_shape, mid_shape)
         dense_out = nn.Linear(mid_shape, self.output)
         inner = inner.view(inner.numel())
-        inner = dense_in(inner)
-        inner = F.relu(inner)
+        inner = F.relu(dense_in(inner))
         output = dense_out(inner)
         return output
 
 if __name__ == '__main__':
     cnn = Net(1, 3, 100, 75, 10)
-    input = Variable(torch.randn(1, 3, 100, 75))
+    input = torch.randn(1, 3, 100, 75)
+    input = Variable(input)
+    criterion = nn.CrossEntropyLoss()
+
+    optimizer = optim.SGD(cnn.parameters(),
+                          lr=0.001,
+                          momentum=0.9)
+
+    optimizer.zero_grad()
+    label = Variable(torch.LongTensor([3]))
+
     output = cnn(input)
-    print(output)
+    # Reshape for output
+    output = output.view(-1, 10)
+    loss = criterion(output, label)
+    loss.backward()
+    optimizer.step()
+
+    print(loss.data[0])
